c014a4693639e6282c759476d9bc953a9d28982b
rodcorral_PC <rcorral1989@gmail.com>
2020-10-12 19:21:46 -0300

    1º commit

---
+++ Assets/Src/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour, IPointerClickHandler
+{
+    public float FlyingSpeed = 2;
+    bool flying = false;
+    Rigidbody2D rigid = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (flying) {
+            transform.position += transform.up * FlyingSpeed * Time.deltaTime;
+        }
+    }
+    
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (flying && collision.collider.GetComponent<Asteroid>() != null) {
+
+            int count = collision.contactCount;
+            Vector2 contact = Vector2.zero;
+
+            for (int i = 0; i < count; i++) {
+                contact += collision.GetContact(i).point;
+            }
+            contact /= count;
+
+            flying = false;
+            transform.position = contact;
+            transform.up = transform.position - collision.transform.position;
+            transform.SetParent(collision.transform);
+        }
+    }
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 0.3f);
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * 2);
+    }
+#endif
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("OnTriggerEnter2D " + collision.name);
+        
+        if (flying && collision.GetComponent<Limits>() != null) {
+            RestartGame();
+        }
+    }
+
+    void RestartGame() {
+        Debug.Log("Restart");
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData) {
+        Debug.Log("Click");
+        if (!flying) {
+            Jump();
+        }
+    }
+
+    void Jump() {
+        flying = true;
+        transform.SetParent(null);
+        rigid.velocity = Vector2.zero;
+        rigid.angularVelocity = 0;
+    }
+}
---
+++ Assets/Src/PuginTester.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PuginTester : MonoBehaviour {
+
+    [SerializeField] Text outputText;
+
+
+    const string PLUGUIN_NAME = "com.imagecampus.rodrigounitylib";
+    static AndroidJavaClass _pluginClass = null;
+    public static AndroidJavaClass PluginClass {
+        get {
+            if (_pluginClass == null)
+                _pluginClass = new AndroidJavaClass(PLUGUIN_NAME);
+            return _pluginClass;
+        }
+    }
+
+    static AndroidJavaObject _pluginInstace = null;
+    public AndroidJavaObject PluginInstace {
+        get {
+            if (_pluginInstace == null) {
+                _pluginInstace = PluginClass.CallStatic<AndroidJavaObject>("getInstance");
+            }
+            return _pluginInstace;
+        }
+    }
+
+    string GetInstanceTime() {
+        return PluginInstace.Call<string>("getLocalTime");
+    }
+
+    string GetFullTime() {
+        return PluginClass.CallStatic<string>("getFullTime");
+    }
+
+    public void TestPluginBtn() {
+        if (Application.platform != RuntimePlatform.Android) {
+            Debug.LogWarning("You are not in android platform");
+            outputText.text = "You are not in android platform";
+            return;
+        }
+
+        string localTime = GetInstanceTime();
+        Debug.Log(localTime);
+        string fullTime = GetFullTime();
+        Debug.Log(fullTime);
+        outputText.text = $"local time: {localTime}" +
+            $"\n" +
+            $"full time: {fullTime}";
+    }
+}
