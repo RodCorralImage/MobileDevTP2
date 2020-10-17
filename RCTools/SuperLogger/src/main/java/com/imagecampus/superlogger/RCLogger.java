880c893f3b7b727d5cd46307f351ec6c1e16cb96
rodcorral_PC <rcorral1989@gmail.com>
2020-10-17 17:53:15 -0300

    Creado la clase del logger

---
+++ RCTools/SuperLogger/src/main/java/com/imagecampus/superlogger/RCLogger.java
@@ -0,0 +1,36 @@
+package com.imagecampus.superlogger;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RCLogger {
+    private static final String LOGGER_TAG = "RCLogger";
+    private static final String GAME_TAG = "TPNro2";
+
+    private static RCLogger _instance = null;
+    public static RCLogger getInstance() {
+        if (_instance == null) {
+            Log.d(LOGGER_TAG, "RCLogger created");
+            _instance = new RCLogger();
+        }
+        return _instance;
+    }
+
+
+    private ArrayList<String> allLogs = new ArrayList<String>();
+
+    public void SendLog(String msj) {
+        Log.d(GAME_TAG, msj);
+        allLogs.add(msj);
+    }
+
+    private static final String SEPARATOR = "\n";
+    public String GetAllLogs() {
+        String logs = "";
+        for (int i = 0; i < allLogs.size(); i++)
+            logs += allLogs.get(i) + SEPARATOR;
+        return logs;
+    }
+}
